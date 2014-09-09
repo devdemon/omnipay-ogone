e9db5c80b4ba5ecd24a60c65d4ff69ce11a2d44f
Chris Hardee <shazzner@gmail.com>
2014-09-09 14:42:35 -0500

    Implimenting the requested data

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -2,19 +2,44 @@
 
 namespace Omnipay\Ogone\Message;
 
+use Omnipay\Common\Message\AbstractRequest;
+
 /**
  * OGone Authorize Request
  */
 class AuthorizeRequest extends AbstractRequest
 {
-    protected $action = 'AUTH_ONLY';
+
+    protected $liveEndpoint = 'https://secure.ogone.com/ncol/prod/orderstandard(_utf8).asp';
+    protected $developerEndpoint = 'https://secure.ogone.com/ncol/test/orderstandard(_utf8).asp';
+
+    public function getApiLoginId()
+    {
+        return $this->getParameter('pspId');
+    }
+
+    public function setApiLoginId($value)
+    {
+        return $this->setParameter('pspId', $value);
+    }
+
+    public function getTestMode()
+    {
+        return $this->getParameter('testMode');
+    }
+
+    public function setTestMode($value)
+    {
+        return $this->setParameter('testMode', $value);
+    }
 
     public function getData()
     {
         $this->validate('amount', 'card');
         $this->getCard()->validate();
 
-        $data = $this->getBaseData();
+        $data = array();
+        $data['PSPID'] = $this->getApiLoginId();
         $data['x_customer_ip'] = $this->getClientIp();
         $data['x_card_num'] = $this->getCard()->getNumber();
         $data['x_exp_date'] = $this->getCard()->getExpiryDate('my');
