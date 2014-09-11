e5a018e0e2b90531d5cefdbdd1bd41a75c62c53d
Chris Hardee <shazzner@gmail.com>
2014-09-11 16:37:43 -0500

    Authorize Reponse (forgot to add)

---
+++ src/Message/AuthorizeResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Omnipay\Ogone\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
+
+/**
+ * Ogone Purchase Response
+ * Based off the Wordpay Response
+ */
+class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
+{
+    public function isSuccessful()
+    {
+        return false;
+    }
+
+    public function isRedirect()
+    {
+        return true;
+    }
+
+    public function getRedirectUrl()
+    {
+        return $this->getRequest()->getEndpoint().'?'.http_build_query($this->data);
+    }
+
+    public function getRedirectMethod()
+    {
+        return 'GET';
+    }
+
+    public function getRedirectData()
+    {
+        return null;
+    }
+}
