f3927fe6711e1f94f9efda4111cfc0c198035019
Chris Hardee <shazzner@gmail.com>
2014-09-23 09:47:25 -0500

    Initial complete purchase request

--- src/Message/EcommerceCompletePurchaseRequest.php
+++ src/Message/EcommerceCompletePurchaseRequest.php
@@ -2,6 +2,7 @@
 
 namespace Omnipay\Ogone\Message;
 
+use Omnipay\Common\Message\AbstractRequest;
 use Omnipay\Common\Exception\InvalidRequestException;
 
 /**
@@ -9,5 +10,20 @@ use Omnipay\Common\Exception\InvalidRequestException;
  */
 class EcommerceCompletePurchaseRequest extends AbstractRequest
 {
+
+    public function getData()
+    {
+        $callbackPW = (string) $this->httpRequest->request->get('callbackPW');
+        if ($callbackPW !== $this->getCallbackPassword()) {
+            throw new InvalidResponseException("Invalid callback password");
+        }
+
+        return $this->httpRequest->request->all();
+    }
+
+    public function sendData($data)
+    {
+        return $this->response = new CompletePurchaseResponse($this, $data);
+    }
 
 }
